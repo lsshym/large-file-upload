b1ff42e67d900eea3a21242270e67ed9f98f9312
Lssh <105506221+lsshym@users.noreply.github.com>
2024-08-31 21:23:21 +0800

    删除打包时输出到type目录下

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { visualizer } from 'rollup-plugin-visualizer';
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   build: {
     lib: {
       entry: "./lib/main.ts",
-      name: "FileChunksTools", // 库的全局变量名称（用于 UMD/IIFE 构建）
+      name: "file-chunks-tools", // 库的全局变量名称（用于 UMD/IIFE 构建）
       fileName: "file-chunks-tools", // 输出文件名，基于不同格式生成文件
     },
   },
   plugins: [
     dts({
       include: ["./lib/**/*"],
-      outDir: "./dist/types",
       compilerOptions: {
         declaration: true, // 启用声明文件生成
         emitDeclarationOnly: true, // 只生成声明文件，不生成 .js 文件
@@ -21,6 +20,6 @@ export default defineConfig({
         forceConsistentCasingInFileNames: true,
       },
     }),
-    visualizer()
+    visualizer(),
   ],
 });
