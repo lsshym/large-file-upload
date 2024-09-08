72c631c6c26c33bd9a04e0fe33642474550eb835
Lssh <105506221+lsshym@users.noreply.github.com>
2024-09-08 13:26:03 +0800

    格式化时忽略dist

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
-    ignores: ['src/main.ts'],
+    ignores: ['src/main.ts', 'dist'],
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
