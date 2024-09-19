9e4a8f5010fb72276b0611ad50282f3a40da2cbb
Lssh <105506221+lsshym@users.noreply.github.com>
2024-09-20 07:48:37 +0800

    删除生成uuid的方法
    有这时间完全没必要去安装这个库，还不如自己手写

--- lib/apis/generateIdUtils.ts
+++ lib/apis/generateIdUtils.ts
@@ -1,42 +1,5 @@
 import { currentFileChunks, FileChunkResult } from './currentFileChunks';
 import { WorkerLabelsEnum } from './worker/worker.enum';
-/**
- * Generate a Universally Unique Identifier (UUID) version 4.
- * This function uses the crypto API of the browser to generate a secure random number,
- * ensuring the uniqueness and randomness of the UUID.
- * The generated UUID conforms to the format of UUID version 4.
- *
- * If there is no need for fast file uploads, this function can be used.
- *
- * @returns {string} - A string representing the generated UUID.
- */
-export function generateUUID() {
-  if (window.crypto && window.crypto.getRandomValues) {
-    // 使用crypto API生成安全的随机数
-    const arr = new Uint8Array(16);
-    window.crypto.getRandomValues(arr);
-
-    // 按照UUID v4的标准格式化数组中的字节
-    arr[6] = (arr[6] & 0x0f) | 0x40; // 第6个字节的高4位设置为4
-    arr[8] = (arr[8] & 0x3f) | 0x80; // 第8个字节的高2位设置为10
-
-    // 将字节转换为符合UUID格式的字符串
-    return [...arr]
-      .map((b, i) =>
-        [4, 6, 8, 10].includes(i)
-          ? '-' + b.toString(16).padStart(2, '0')
-          : b.toString(16).padStart(2, '0'),
-      )
-      .join('');
-  } else {
-    // 如果不支持crypto API，则回退到Math.random生成UUID
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-      const r = (Math.random() * 16) | 0;
-      const v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
-}
 
 export interface FileHashResult {
   hash: string;
--- lib/main.ts
+++ lib/main.ts
@@ -1,14 +1,12 @@
 import { PromisePool } from './apis/promisePool';
 import {
   generateFileHash,
-  generateUUID,
 } from './apis/generateIdUtils';
 import { currentFileChunks } from './apis/currentFileChunks';
 import { uploadChunksWithPool } from './apis/upload.helper';
 export {
   currentFileChunks,
   generateFileHash,
-  generateUUID,
   uploadChunksWithPool,
   PromisePool,
 };
