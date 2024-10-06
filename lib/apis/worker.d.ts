48ce4768782a151d03178d621c9c45a09b6f100b
Lssh <105506221+lsshym@users.noreply.github.com>
2024-10-06 22:27:44 +0800

    Update worker.d.ts

--- lib/apis/worker.d.ts
+++ lib/apis/worker.d.ts
@@ -1,7 +1,3 @@
-// declare module 'worker-loader!*' {
-//     const WorkerFactory: new () => Worker;
-//     export default WorkerFactory;
-//   }
 declare module '*?worker' {
   const WorkerFactory: new () => Worker;
   export default WorkerFactory;
