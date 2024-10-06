f719b2f280bfca05155d74d7b0f7bf5c4c0eb279
Lssh <105506221+lsshym@users.noreply.github.com>
2024-10-06 11:25:04 +0800

    Update main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,3 @@
-import { UploadHelper } from './apis/upload.helper.class';
-import { generateFileHash } from './apis/generateIdUtils';
-import { currentFileChunks } from './apis/currentFileChunks';
-export { currentFileChunks, generateFileHash, UploadHelper };
+export { currentFileChunks } from './apis/currentFileChunks';
+export { generateFileHash } from './apis/generateIdUtils';
+export { UploadHelper } from './apis/upload.helper.class';
