8a750e35cd825c87151f07947e9766174058fba9
Lssh <105506221+lsshym@users.noreply.github.com>
2024-10-06 16:03:39 +0800

    Update generateIdUtils.ts

--- lib/apis/generateIdUtils.ts
+++ lib/apis/generateIdUtils.ts
@@ -1,5 +1,4 @@
 import { currentFileChunks, FileChunkResult } from './currentFileChunks';
-import Md5Worker from './md5.workers.ts?worker';
 
 export enum WorkerLabelsEnum {
   DOING = 'DOING',
@@ -40,10 +39,9 @@ export function generateFileHash(file: File, customChunkSize?: number): Promise<
 
     try {
       for (let i = 0; i < workerCount; i++) {
-        const worker = new Md5Worker();
-        // const worker = new Worker(new URL('./md5.workers.ts', import.meta.url), {
-        //   type: 'module',
-        // });
+        const worker = new Worker(new URL('./md5.workers.ts', import.meta.url), {
+          type: 'module',
+        });
         workers.push(worker);
         worker.onmessage = (event: MessageEvent) => {
           const { label, data, index } = event.data;
