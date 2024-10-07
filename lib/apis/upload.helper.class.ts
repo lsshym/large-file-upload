3f8dfe5d61fd8ea0f1056a96922ef7a6ea2f3c4f
Lssh <105506221+lsshym@users.noreply.github.com>
2024-10-07 21:35:31 +0800

    Update upload.helper.class.ts
    重构

--- lib/apis/upload.helper.class.ts
+++ lib/apis/upload.helper.class.ts
@@ -1,3 +1,8 @@
+enum TaskState {
+  RUNNING,
+  PAUSED,
+}
+
 import { SimpleBehaviorSubject } from './simpleObservable';
 
 // 定义异步函数的类型，返回 Promise 类型
@@ -8,117 +13,121 @@ export class UploadHelper<T> {
   private maxConcurrentTasks: number;
   private results: (T | Error)[] = [];
   private currentRunningCount = new SimpleBehaviorSubject(0);
-  private isPaused = false;
+  private taskState: TaskState = TaskState.RUNNING;
   private _currentTaskIndex = 0; // 私有属性，用于记录当前任务索引
-  private controllers: {
-    [index: number]: { fn: AsyncFunction<T>; controller: AbortController; index: number };
-  } = {};
+  private controllers: Map<number, { controller: AbortController; fn: AsyncFunction<T> }> =
+    new Map();
   private subscription: { unsubscribe: () => void } | null = null;
+
   private maxErrors: number; // 最大允许错误数
   private errorCount = 0; // 当前错误数
   private indexChangeListener: ((index: number) => void) | null = null; // 任务索引变化监听器
+  private stopOnMaxErrors: boolean;
 
   constructor(
     functions: AsyncFunction<T>[],
     maxConcurrentTasks: number = navigator.hardwareConcurrency || 4,
     maxErrors: number = 10, // 默认最大错误数为 10
+    stopOnMaxErrors: boolean = true, // 是否在达到最大错误数后停止任务
   ) {
     this.queue = functions.map((fn, index) => ({ fn, index }));
     this.maxConcurrentTasks = maxConcurrentTasks;
     this.maxErrors = maxErrors;
+    this.stopOnMaxErrors = stopOnMaxErrors;
   }
 
   // 执行任务队列
   exec(): Promise<T[]> {
-    return new Promise<T[]>((resolve, reject) => {
-      const processQueue = () => {
-        if (this.isPaused || this.currentRunningCount.value >= this.maxConcurrentTasks) {
-          return;
-        }
-
-        if (this.queue.length === 0 && this.currentRunningCount.value === 0) {
-          resolve(this.results as T[]);
-          return;
-        }
-
-        if (this.errorCount > this.maxErrors) {
-          reject(new Error('Error limit exceeded'));
-          return;
-        }
-
-        while (
-          !this.isPaused &&
-          this.currentRunningCount.value < this.maxConcurrentTasks &&
-          this.queue.length > 0
-        ) {
-          const { fn, index } = this.queue.shift()!;
-          const controller = new AbortController();
-          this.controllers[index] = { controller, fn, index };
-          this.currentRunningCount.next(this.currentRunningCount.value + 1);
-
-          fn({ signal: controller.signal })
-            .then(result => {
-              this.results[index] = result;
-            })
-            .catch(error => {
-              this.results[index] = error;
-              this.errorCount++;
-              if (this.errorCount > this.maxErrors) {
-                reject(new Error('Error limit exceeded'));
-              }
-            })
-            .finally(() => {
-              this.currentRunningCount.next(this.currentRunningCount.value - 1);
-              delete this.controllers[index];
-              this._currentTaskIndex++;
-              this.notifyIndexChange();
-            });
-        }
-      };
-
+    return new Promise(resolve => {
       this.subscription = this.currentRunningCount.subscribe(() => {
-        processQueue();
+        this.processQueue();
+        if (this.currentRunningCount.value === 0 && this.queue.length === 0) {
+          this.controllers.clear();
+          this.unsubscribe();
+          resolve(this.results as T[]);
+        }
       });
-
-      processQueue();
-    }).finally(() => {
-      this.unsubscribe();
-      this.controllers = {};
     });
+  }
+
+  // 处理任务队列
+  private processQueue(): void {
+    if (
+      this.taskState === TaskState.PAUSED ||
+      this.currentRunningCount.value >= this.maxConcurrentTasks
+    ) {
+      return;
+    }
+
+    if (this.queue.length === 0 && this.currentRunningCount.value === 0) {
+      return;
+    }
+
+    if (this.errorCount > this.maxErrors && this.stopOnMaxErrors) {
+      this.cancelAll();
+      return;
+    }
+
+    while (
+      this.taskState === TaskState.RUNNING &&
+      this.currentRunningCount.value < this.maxConcurrentTasks &&
+      this.queue.length > 0
+    ) {
+      const { fn, index } = this.queue.shift()!;
+      const controller = new AbortController();
+      this.controllers.set(index, { controller, fn });
+      this.currentRunningCount.next(this.currentRunningCount.value + 1);
+      this.runTask(fn, index, controller);
+    }
+  }
+
+  // 运行单个任务
+  private runTask(fn: AsyncFunction<T>, index: number, controller: AbortController): void {
+    fn({ signal: controller.signal })
+      .then(result => {
+        this.results[index] = result;
+      })
+      .catch(error => {
+        this.results[index] = error;
+        this.errorCount++;
+        if (this.errorCount > this.maxErrors && this.stopOnMaxErrors) {
+          this.cancelAll();
+        }
+      })
+      .finally(() => {
+        this.currentRunningCount.next(this.currentRunningCount.value - 1);
+        this.controllers.delete(index);
+        this._currentTaskIndex++;
+        this.notifyIndexChange();
+      });
   }
 
   // 暂停任务
   pause() {
-    this.isPaused = true;
-    Object.values(this.controllers).forEach(({ controller, fn, index }) => {
+    this.taskState = TaskState.PAUSED;
+    this.controllers.forEach(({ controller, fn }, index) => {
       controller.abort();
-      this.queue.unshift({ fn, index });
+      this.queue.push({ fn, index });
     });
-    this.controllers = {};
+    this.controllers.clear();
   }
 
   // 恢复任务
   resume() {
-    if (this.isPaused) {
-      this.isPaused = false;
-      this.currentRunningCount.next(this.currentRunningCount.value);
+    if (this.taskState === TaskState.PAUSED) {
+      this.taskState = TaskState.RUNNING;
+      this.processQueue();
     }
   }
 
   // 取消所有任务
   cancelAll() {
-    this.isPaused = true;
-    Object.values(this.controllers).forEach(({ controller }) => {
+    this.controllers.forEach(({ controller }) => {
       controller.abort();
     });
-    this.controllers = {};
+    this.controllers.clear();
     this.queue = [];
     this.currentRunningCount.next(0);
-  }
-
-  // 获取当前任务索引
-  get currentTaskIndex(): number {
-    return this._currentTaskIndex;
   }
 
   // 设置任务索引变化的监听器
