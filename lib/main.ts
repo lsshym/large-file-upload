826ac6d883e33cd6585128bbe0e7100fe53df2e5
Lssh <105506221+lsshym@users.noreply.github.com>
2024-11-01 09:54:12 +0800

    Update main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,4 @@
-// 导出时使用动态导入，生成可分割的代码块
-export const generateFileHash = () => import('./apis/generateIdUtils/generateFileHash');
-export const createFileChunks = () => import('./apis/createFileChunks');
-export const UploadHelper = () => import('./apis/uploadHelper/upload.helper.mainThread');
-
-// export { generateFileHash, createFileChunks, UploadHelper };
+import { createFileChunks } from './apis/createFileChunks';
+import { generateFileHash } from './apis/generateIdUtils/generateFileHash';
+import { UploadHelper } from './apis/uploadHelper/upload.helper.mainThread';
+export { generateFileHash, createFileChunks, UploadHelper };
